f44377532b32cda4e824c1604553949a39f6fab3
ProjectoUlysses <32078011+ProjectoUlysses@users.noreply.github.com>
2017-12-14 23:38:05 -0200

    Add files via upload

---
+++ Software versões/Tese_software_V_1.8/leitura_i2c_v_1.0/leitura_i2c_v_1.0.ino
@@ -0,0 +1,45 @@
+#include <Wire.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Wire.begin(8);
+  Wire.onReceive(receiveEvent);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+byte byte1, byte2, byte3, byte4;
+char c;
+unsigned int aux;
+int x;
+float aux_saida;
+
+void receiveEvent(int bytes) {
+  
+  x = Wire.read();    // recebe o byte como inteiro
+  Serial.print("SOC: ");
+  Serial.print(x);
+  Serial.println("%"); 
+  
+  Serial.print("Corrente: ");
+   int corrente = Wire.read();
+   float resultado = (float)corrente/100;
+     
+   Serial.print(resultado);
+   Serial.println(" A");
+   
+  Serial.print("Corrente consumida: ");
+   int correntec = Wire.read();
+   float resultado2 = (float)correntec/100;
+   
+   Serial.print(resultado2);
+   Serial.println(" Ah");
+   
+  Serial.print("Temperatura da bateria: ");
+  x = Wire.read();    // recebe o byte como inteiro
+  Serial.print(x); 
+  Serial.println(" Graus");
+}
